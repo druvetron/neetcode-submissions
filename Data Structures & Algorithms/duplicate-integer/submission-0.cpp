5495b3ea20c3c673be00b71fbdcc82ab454f240a
dhruv <159679178+druvetron@users.noreply.github.com>
2026-08-29 12:17:13 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mpp ;
+
+        for(int i : nums){
+            mpp[i]++ ;
+            if(mpp[i]>1){
+                return true ;
+            }
+        }
+
+        return false ;
+    }
+};
